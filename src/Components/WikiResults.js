b4b0c1621219a268c62fe9dacf5733146937f00d
Eric R. Kock <eric.r.kock@gmail.com>
2019-09-24 19:45:07 +0100

    Api update

--- src/Components/WikiResults.js
+++ src/Components/WikiResults.js
@@ -3,24 +3,16 @@ import './WikiResults.css';
 
 class WikiResults extends React.Component {
    render() {
-      const { error, wikiItems } = this.props;
-      if (error) {
-         return <div>Error: {error.message}</div>
-      } else {
-
-         return (
-            <div className="result-wrapper">
-               <ul>
-                  {wikiItems.map(item => (
-                     <li key={item} >
-                        {item}
-                     </li>
-                  ))}
-               </ul>
-            </div>
-         );
-      }
+      return (
+         <div className="result-wrapper">
+            <ul>
+               {this.props.wikiItems && 
+                  <li>{this.props.wikiItems}</li>}
+            </ul>
+         </div>
+      );
    }
 }
+
 
 export default WikiResults;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './index.css';
 
 import ProjectHeader from './Components/ProjectHeader';
@@ -12,11 +13,19 @@ class WikepediaViewer extends React.Component {
       super(props);
       this.state = {
          wikiSearch: "",
-         button: "SUBMIT",
+         button: "SEARCH",
          variant: "outline-dark",
-         error: null,
-         isLoaded: false,
-         wikiItems: []
+         SearchResult01: "",
+         SearchResult02: "",
+         SearchResult03: "",
+         SearchResult04: "",
+         SearchResult05: "",
+         SearchResult06: "",
+         SearchResult07: "",
+         SearchResult08: "",
+         SearchResult09: "",
+         SearchResult10: "",
+         wikiItems: undefined
       };
       this.handleChange = this.handleChange.bind(this);
    }
@@ -25,37 +34,44 @@ class WikepediaViewer extends React.Component {
       this.setState({ wikiSearch: event.target.value });
    }
 
-
-
    search = () => {
-      if (this.state.button === "SUBMIT") {
-         this.setState({
-            button: "RESET",
-            variant: "warning",
+      if (this.state.wikiItems != undefined || this.state.wikiSearch != "") {
+         const keyWord = this.state.wikiSearch;
+         const url = `https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${keyWord}&callback=?`
+         axios.get(url).then((res) => {
+            const data = res.data;
+               if (this.state.button === "SEARCH") {
+                  for (let x = 0; x <= 10; x++) {
+                     this.setState({
+                        SearchResult01: data[3][i]
+                     })
+                  }
+               this.setState({
+                  button: "RESET",
+                  variant: "warning",
+                  wikiItems: data
+               });
+            } else {
+               this.setState({
+                  wikiSearch: "",
+                  button: "SEARCH",
+                  variant: "outline-dark",
+                  SearchResult01: "",
+                  SearchResult02: "",
+                  SearchResult03: "",
+                  SearchResult04: "",
+                  SearchResult05: "",
+                  SearchResult06: "",
+                  SearchResult07: "",
+                  SearchResult08: "",
+                  SearchResult09: "",
+                  SearchResult10: "",
+                  wikiItems: undefined
+               });
+            };   
          });
-         fetch("https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=" + this.state.wikiSearch + "&callback=?")
-            .then(res => res.json())
-            .then(
-               (result) => {
-                  console.log("Result:", result);
-                  this.setState({
-                     isLoaded: true,
-                     wikiItems: result
-                  });
-               },
-               (error) => {
-                  this.setState({
-                     isLoaded: true,
-                     error
-                  });
-               }
-            )
       } else {
-         this.setState({
-            wikiSearch: "",
-            button: "SUBMIT",
-            variant: "outline-dark"
-         });
+         this.setState({wikiItems: "Enter a Keyword ..."})
       }
    }
 
