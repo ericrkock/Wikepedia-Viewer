779092a1b205e2eb83100b53147aaba031998e4c
Eric R. Kock <eric.r.kock@gmail.com>
2019-10-07 15:06:16 +0100

    Updates

---
+++ precache-manifest.e5893d0d52e3ca4796419f35ef4a3dfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78810aeb97da39b1d2fa7631d9d585de",
+    "url": "/Wikipedia-Viewer/index.html"
+  },
+  {
+    "revision": "b98191402fff9cd27966",
+    "url": "/Wikipedia-Viewer/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4dddcb6b96237de8acdf",
+    "url": "/Wikipedia-Viewer/static/css/main.e97a79be.chunk.css"
+  },
+  {
+    "revision": "b98191402fff9cd27966",
+    "url": "/Wikipedia-Viewer/static/js/2.a607076b.chunk.js"
+  },
+  {
+    "revision": "4dddcb6b96237de8acdf",
+    "url": "/Wikipedia-Viewer/static/js/main.d9adfd01.chunk.js"
+  },
+  {
+    "revision": "742cc6b782a22d53c631",
+    "url": "/Wikipedia-Viewer/static/js/runtime~main.496ee28c.js"
+  }
+]);
